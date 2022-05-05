2c1068b66556cc783e5a5ba58b47377422fdfbad
romariou76 <illeniumr@gmail.com>
2022-05-04 21:14:44 -0500

    Agregando estilos css a los componentes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css'
 import Testimonio from './componentes/Testimonio.js';
 function App() {                //este contenedor esta renderizando el componente testimonio
   return (
-    <div className="App">  
-      <Testimonio /> 
+    <div className="App"> 
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1> 
+        <Testimonio />
+      </div>
     </div>
   );
 }
